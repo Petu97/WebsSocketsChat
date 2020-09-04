7ae1d7424154e814d61f6d42d4b6a34f3c9f1aae
Petu <60960104+Petu97@users.noreply.github.com>
2020-09-04 20:35:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const io = require('socket.io')(3000)
+const io = require("socket.io")(3000);
 
-const users = {}
-var userList = []
-var userToRemove = ''
+const users = {};
+var userList = [];
+var userToRemove = "";
 
-
-
-
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    if (name == null || name == "")
-        name = "anonymous"
-    console.log("user connected")
-    users[socket.id] = name
-    userList.push(name)
-    //These are ran for the client joining 
-    socket.emit('user-profile', name);
-    socket.emit('online-users-list', userList)
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    if (name == null || name == "") name = "anonymous";
+    console.log("user connected");
+    users[socket.id] = name;
+    userList.push(name);
+    //These are ran for the client joining
+    socket.emit("user-profile", name);
+    socket.emit("online-users-list", userList);
     //Broadcasted to everyone
-    socket.broadcast.emit('user-connected', name)
-    socket.broadcast.emit('online-users-list', userList)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-      socket.broadcast.emit('user-disconnected', users[socket.id])
-      userToRemove = users[socket.id.toString()]
-      delete users[socket.id]
-      userList = userList.filter(item => item !== userToRemove)
-      socket.broadcast.emit('online-users-list', userList)
-      userToRemove = ''
-  })
-})
+    socket.broadcast.emit("user-connected", name);
+    socket.broadcast.emit("online-users-list", userList);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    userToRemove = users[socket.id.toString()];
+    delete users[socket.id];
+    userList = userList.filter((item) => item !== userToRemove);
+    socket.broadcast.emit("online-users-list", userList);
+    userToRemove = "";
+  });
+});
